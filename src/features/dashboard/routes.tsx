549fc6a2c9d5dcf99f5bd7b5815a0d58cc44eff2
Aman Sadhwani <aman.sadhwani@amerisourcebergen.com>
2026-07-14 12:38:16 -0300

    AS-40-cleanup-rbac

--- src/features/dashboard/routes.tsx
+++ src/features/dashboard/routes.tsx
@@ -1,10 +1,14 @@
 import type { RouteObject } from "react-router-dom";
 
 import { APP_ROUTES } from "@/app/config";
+import { Permission } from "@/auth";
 
 export const dashboardRoutes: RouteObject[] = [
   {
     path: APP_ROUTES.DASHBOARD,
+    handle: {
+      permission: Permission.DashboardRead,
+    },
     async lazy() {
       const module = await import("./pages/DashboardPage");
       return {
--- src/features/users/routes.tsx
+++ src/features/users/routes.tsx
@@ -1,10 +1,14 @@
 import type { RouteObject } from "react-router-dom";
 
 import { APP_ROUTES } from "@/app/config";
+import { Permission } from "@/auth";
 
 export const usersRoutes: RouteObject[] = [
   {
     path: APP_ROUTES.USERS,
+    handle: {
+      permission: Permission.UsersRead,
+    },
     async lazy() {
       const module = await import("./pages/UsersPage");
 
